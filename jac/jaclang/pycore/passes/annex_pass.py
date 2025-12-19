2064588a94db604eeec26498a1d5f75192d0f463
Jason Mars <jason@mars.ninja>
2025-12-19 17:45:37 -0500

    Added new impl organization option and applied it to codebase (#3793)

--- jac/jaclang/pycore/passes/annex_pass.py
+++ jac/jaclang/pycore/passes/annex_pass.py
@@ -9,6 +9,11 @@ implementations or tests for a base module. The pass:
 3. Attaches them to the appropriate base module
 4. Maintains proper relationships between base modules and their annexes
 
+Impl files are discovered from:
+- Same directory: foo.impl.jac for foo.jac
+- Module-specific folder: foo.impl/bar.impl.jac for foo.jac
+- Shared impl folder: impl/foo.impl.jac for foo.jac
+
 This enables the separation of interface and implementation, as well as test code organization.
 """
 
@@ -39,13 +44,19 @@ class JacAnnexPass(Transform[uni.Module, uni.Module]):
         self.test_folder = self.base_path + ".test"
         self.cl_folder = self.base_path + ".cl"
         self.directory = os.path.dirname(self.mod_path) or os.getcwd()
+        self.shared_impl_folder = os.path.join(self.directory, "impl")
         self.load_annexes(jac_program=self.prog, node=ir_in)
         return ir_in
 
     def find_annex_paths(self) -> list[str]:
         """Return list of .impl.jac and .test.jac files related to base module."""
         paths = [os.path.join(self.directory, f) for f in os.listdir(self.directory)]
-        for folder in [self.impl_folder, self.test_folder, self.cl_folder]:
+        for folder in [
+            self.impl_folder,
+            self.test_folder,
+            self.cl_folder,
+            self.shared_impl_folder,
+        ]:
             if os.path.exists(folder):
                 paths += [os.path.join(folder, f) for f in os.listdir(folder)]
         return paths
@@ -58,15 +69,19 @@ class JacAnnexPass(Transform[uni.Module, uni.Module]):
             self.log_error("Module path is empty.")
             return
 
+        base_name = os.path.basename(self.base_path)
         for path in self.find_annex_paths():
             if path == self.mod_path:
                 continue
-
             if (
                 path.endswith(".impl.jac")
                 and (
                     path.startswith(f"{self.base_path}.")
                     or os.path.dirname(path) == self.impl_folder
+                    or (
+                        os.path.dirname(path) == self.shared_impl_folder
+                        and os.path.basename(path).startswith(f"{base_name}.")
+                    )
                 )
                 or path.endswith(".cl.jac")
                 and (
