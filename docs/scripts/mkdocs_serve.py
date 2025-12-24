9e7e5c6163252d3aa4c65edc6c84c9e3b59c9c76
Malitha Prabhashana <110366097+MalithaPrabhashana@users.noreply.github.com>
2025-12-24 18:57:50 +0530

    mkdocs `yml` path fixed  (#3834)
    <img width="754" height="384" alt="image"
    src="https://github.com/user-attachments/assets/2e708f92-317f-42e5-9deb-8e27b83aaf9f"
    />

--- docs/scripts/mkdocs_serve.py
+++ docs/scripts/mkdocs_serve.py
@@ -243,15 +243,14 @@ def serve_with_watch() -> None:
     """Serve MkDocs site and watch for file changes to trigger rebuilds."""
     port = 8000
     root_dir = os.path.dirname(os.path.dirname(__file__))
-    yml_dir = os.path.abspath(os.path.join(root_dir, ".."))
-    site_dir = os.path.join(yml_dir, "site")
+    site_dir = os.path.join(root_dir, "site")
     ignore_paths = [
         os.path.join(root_dir, "docs", "assets"),
         os.path.join(root_dir, "docs", "playground", "jaclang.zip"),
     ]
 
     print("Initial build of MkDocs site...")
-    subprocess.run(["mkdocs", "build"], check=True, cwd=yml_dir)
+    subprocess.run(["mkdocs", "build"], check=True, cwd=root_dir)
     reload_queue.put_nowait("reload")
 
     # Set up file watcher
